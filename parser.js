aa9242c168e542f961d4a4123a32d41c7b53ea5d
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2016-02-22 14:30:22 +0000

    define 'classes'

--- parser.js
+++ parser.js
@@ -1,4 +1,30 @@
-var Point = function(x, y) {
+var Coordinate = function(x, y) {
 	this.x = x; 
 	this.y = y;
+};
+
+var CheckablePolygon = function(vertices, guards) {
+	this.vertices = vertices;
+	this.guards = guards;
+}
+
+// Return [CheckablePolygon]
+var getPolygonAndGuards = function(file, callback) {
+	var reader = new FileReader();
+	reader.onload = function(event) {
+		var lines = reader.result.split(/\r?\n/);
+		var polygons = [];
+		for(var polygon = 0 ; polygon < lines.length ; polygon++) {
+			var line = lines[i];
+			line.replace(/\d+: /, "");
+			var vertices = [];
+			var vertexCoordinates = line.match(/\([^\)]+\)/g);
+			for(var j = 0 ; j < vertexCoordinates.length ; j++) {
+				var coordinateString = vertexCoordinates[j];
+				var points = coordinateString.match(/-?\d+(\.\d+)?/g);
+				vertices.push(Coordinate(points[0], points[1]));
+			}
+		};
+	};
+	reader.readAsText(file);	
 }
